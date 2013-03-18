e5d64651a39995f45cd76ac6d01376bf430b7c22
Sharon Leon <sharon.leon@gmail.com>
2013-03-18 23:28:44 +0200

    changes to routes to reflect simple routs such as /login /logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Baseline::Application.routes.draw do
 
   root :to => "home#index"
 
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
 
 # root :to => "home#index"
 
